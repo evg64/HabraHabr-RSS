84d902224293eb1cffdb55b40e30e0a9d7745fb0
Eugene <chumakzhenya@yahoo.com>
2018-02-05 23:11:27 +0300

    minor changes: comments

--- app/src/test/java/com/eugene/shvabr/data/rss/parser/ParsingTest.java
+++ app/src/test/java/com/eugene/shvabr/data/rss/parser/ParsingTest.java
@@ -11,9 +11,7 @@ import org.simpleframework.xml.core.Persister;
 import java.io.InputStream;
 
 /**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * Тест xml-парсинга.
  */
 public class ParsingTest {
 
